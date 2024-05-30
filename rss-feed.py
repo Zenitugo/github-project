804a7558ce941a2a41e183707f4950adf05ddf70
Ugochi Ukaegbu <ugochiukaegbu19@gmail.com>
2024-05-30 19:42:10 +0100

    python script used in creating rss feeds

--- rss-feed.py
+++ rss-feed.py
@@ -25,30 +25,30 @@ with open('rss-feed.yml', 'r') as file:
     channel_element = xml_tree.SubElement(rss_element, 'channel')
 
     link_prefix = yaml_data['link']
-    xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
-    xml_tree.SubElement(channel_element, 'description').text = yaml_data['description']
-    xml_tree.SubElement(channel_element, 'copyright').text = yaml_data['copyright']
-    xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
-    xml_tree.SubElement(channel_element, 'pubDate').text = yaml_data['pubDate']
-    xml_tree.SubElement(channel_element, 'link').text = link_prefix
-    xml_tree.SubElement(channel_element, 'lastBuilddate').text = yaml_data['lastBuildDate']
-    xml_tree.SubElement(channel_element, 'itunes:link').text = yaml_data['link']
+    xml_tree.SubElement(channel_element, 'title').text = str(yaml_data['title'])
+    xml_tree.SubElement(channel_element, 'description').text = str(yaml_data['description'])
+    xml_tree.SubElement(channel_element, 'copyright').text = str(yaml_data['copyright'])
+    xml_tree.SubElement(channel_element, 'language').text = str(yaml_data['language'])
+    xml_tree.SubElement(channel_element, 'pubDate').text = str(yaml_data['pubDate'])
+    xml_tree.SubElement(channel_element, 'link').text = str(link_prefix)
+    xml_tree.SubElement(channel_element, 'lastBuilddate').text = str(yaml_data['lastBuildDate'])
+    xml_tree.SubElement(channel_element, 'itunes:link').text = str(yaml_data['link'])
     xml_tree.SubElement(channel_element, 'itunes:image', {'href': yaml_data['image']})
-    xml_tree.SubElement(channel_element, 'itunes:type').text = yaml_data['itunes:type']
-    xml_tree.SubElement(channel_element, 'itunes:summary').text = yaml_data['itunes:summary']
-    xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['itunes:author']
-    xml_tree.SubElement(channel_element, 'itunes:keyword').text = yaml_data['itunes:keyword']
-    xml_tree.SubElement(channel_element, 'itunes:owner').text = yaml_data['itunes:owner']
-    xml_tree.SubElement(channel_element, 'itunes:type').text = yaml_data['itunes:type']
-    xml_tree.SubElement(channel_element, 'itunes:new-feed-url').text = yaml_data['itunes:new-feed-url']
+    xml_tree.SubElement(channel_element, 'itunes:type').text = str(yaml_data['itunes:type'])
+    xml_tree.SubElement(channel_element, 'itunes:summary').text = str(yaml_data['itunes:summary'])
+    xml_tree.SubElement(channel_element, 'itunes:author').text = str(yaml_data['itunes:author'])
+    xml_tree.SubElement(channel_element, 'itunes:keyword').text = str(yaml_data['itunes:keyword'])
+    xml_tree.SubElement(channel_element, 'itunes:owner').text = str(yaml_data['itunes:owner'])
+    xml_tree.SubElement(channel_element, 'itunes:type').text = str(yaml_data['itunes:type'])
+    xml_tree.SubElement(channel_element, 'itunes:new-feed-url').text = str(yaml_data['itunes:new-feed-url'])
 
 
     for item in yaml_data['item']:
         item_element = xml_tree.SubElement(channel_element, 'item')
-        xml_tree.SubElement(item_element, 'title').text = item['title']
-        xml_tree.SubElement(item_element, 'description').text = item['description']
-        xml_tree.SubElement(item_element, 'pubDate').text = item['pubDate']
-        xml_tree.SubElement(item_element, 'itunes:author').text = item['link']
+        xml_tree.SubElement(item_element, 'title').text = str(item['title'])
+        xml_tree.SubElement(item_element, 'description').text = str(item['description'])
+        xml_tree.SubElement(item_element, 'pubDate').text = str(item['pubDate'])
+        xml_tree.SubElement(item_element, 'itunes:author').text = str(item['link'])
         xml_tree.SubElement(item_element, 'link').text = link_prefix
 
         enclosure = xml_tree.SubElement(item_element, 'enclosure', {
